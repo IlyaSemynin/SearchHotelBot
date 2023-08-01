9b1256f10451168b7325d731b6c659eaa4aaf3c1
Ilya Semynin <sid028@mail.ru>
2023-08-01 22:08:32 +0400

    corrected file gitignore, check result

--- loader.py
+++ loader.py
@@ -1,6 +1,10 @@
+# Подгружаем необходимые модули для создания бота
 from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
 
+#  Создаем глобальную переменную для хранения состояний пользователя внутри сценария
 storage = StateMemoryStorage()
+
+# Создаем сам объект бота
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from loader import bot
-import handlers  # noqa
-from utils.set_bot_commands import set_default_commands
+from loader import bot  # импорт бота из файла loader.py
+import handlers  # noqa импорт папки обработчиков сообщений (хэндлеров)
+from utils.set_bot_commands import set_default_commands  # импорт команд которые будут вызываться из кнопки "Меню"
 
 if __name__ == "__main__":
-    set_default_commands(bot)
-    bot.infinity_polling()  # программа будет работать постоянно
+    set_default_commands(bot)  # установка в кнопку "Меню" команд
+    bot.infinity_polling()  # бесконечный запуск бота
